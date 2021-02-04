71b9ae24859d45b55c2839e125949cde338c0ca4
Dima Demetrashvili <74451049+ddemee@users.noreply.github.com>
2021-02-04 17:52:44 +0400

    Header

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(window).on("scroll", function() {
-    if($(window).scrollTop() > 50) {
+    if($(window).scrollTop() > 90) {
         $(".header").addClass("scrolled");
     } else {
         //remove the background property so it comes transparent again (defined in your css)
