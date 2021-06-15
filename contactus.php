d1af1489bf4d02ce74d8ead37b9502638975259a
ddemee <dimademe69@gmail.com>
2021-06-15 23:34:42 +0400

    dd

--- contactus.php
+++ contactus.php
@@ -1,27 +1,56 @@
-<?php 
-    $name = $_POST ['name'];
-    $lastname = $_POST ['lastname'];
-    $visitor_email = $_POST['mail'];
-    $phone = $_POST['message'];
-    $company = $_POST['companyname'];
-    $message = $_POST['message'];
+<?php
+      if(null!==($_POST["name"])) {
+          $name = $_POST["name"];
+         /* echo "Please enter 'Name'<br />"; */
+      } else {
+          $name = "";
+      }
+      if(null!==($_POST["lastname"])) {
+        $lastname = $_POST["lastname"];
+       /* echo "Please enter 'Name'<br />"; */
+    } else {
+        $lastname = "";
+    }
+      if(null!==($_POST["mail"])) {
+          $email = $_POST["mail"];
+          /* echo "Please enter 'E_Mail'<br />"; */
+      } else {
+          $email = "";
+      }
+      if(null!==($_POST["company"])) {
+        $company = $_POST["company"];
+        /* echo "Please enter 'E_Mail'<br />"; */
+    } else {
+        $company = "";
+    }
+    if(null!==($_POST["phone"])) {
+        $mobile = $_POST["phone"];
+        /* echo "Please enter 'E_Mail'<br />"; */
+    } else {
+        $mobile = "";
+    }
+    if(null!==($_POST["message"])) {
+        $message = $_POST["message"];
+        /* echo "Please enter your message<br />"; */
+    }   else {
+        $message = "";
+    }
+    /*echo "{$name}, {$email}, {$message}";*/
+
+  $email_subject = "New Form submission";
+
+  $email_body = "You have received a new message from the user $name.\n".
+                "Here is the message:\n $lastname";
+                "Here is the email:\n $email";
+                "Here is the company:\n $company";
+                "Here is the phone:\n $phone";
+                "Here is the message:\n $message";
 
 
-    $email_from = 'order@fortinet.ge';
-    $email_subject = 'New Form Submission';
-    $email_body = "User Name:$name.\n".
-                        "User Last Name:$lastname.\n".
-                            "User Number:$phone.\n".
-                                "User Company Name:$company.\n".
-                                    "User Email:$visitor_email.\n".
-                                            "User Message:$message.\n";
-
-
-    $to = "contact@ittech.ge";
-    $headers = "From: $email_from \r\n";
-    $headers .= "Reply To: $visitor_email \r\n";
+  $to = "order@fortinet.ge";
+    $headers = "From: $email \r\n";
+    $headers .= "Reply-To: $email \r\n";
     mail($to,$email_subject,$email_body,$headers);
-    
+    $headers = 'Content-type: text/html; charset=utf-8' . "\r\n";
     header("Location: index.html");
-
-?>
+   ?>
