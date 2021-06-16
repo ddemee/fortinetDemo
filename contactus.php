f0710afab069e01a2680a4fab22c71715d9f741b
ddemee <dimademe69@gmail.com>
2021-06-16 20:04:53 +0400

    dd

--- contactus.php
+++ contactus.php
@@ -1,25 +1,55 @@
 <?php
-// Submit the Form 
-$userName= $_POST['name'];
-$userLast= $_POST['lastname'];
-$userEmail= $_POST['email'];
-$userCompany= $_POST['company'];
-$userPhone= $_POST['phone'];
-$userSelected= $_POST['select'];
-$message= $_POST['message'];
+//  Message Vars 
+$msg = '';
+$msgClass = ' ';
 
-$to = "orders@fortinet.ge"
-$body = "";
 
-$body .= "From: ".$userName. "\r\n";
-$body .= "From: ".$userLast. "\r\n";
-$body .="Email: ".$userEmail. "\r\n";
-$body .="Company: ".$userCompany. "\r\n";
-$body .="Phone: ".$userPhone. "\r\n";
-$body .="Selected: ".$userSelected. "\r\n";
-$body .="message: ".$message. "\r\n";
+if(filter_has_var(INPUT_POST,'submit')){
+    // Gett Form Dataa
+    $name=htmlspecialchars ($_POST['name']);
+    $email= htmlspecialchars ($_POST['email']);
+    $message= htmlspecialchars ($_POST['message']);
 
-mail($to,$body);
-    $headers = 'Content-type: text/html; charset=utf-8' . "\r\n";
-     header("Location: index.html");
-   ?>
+    // Chek Required Fields
+    if(!empty($email) && !empty($name) && !empty($message)){
+        // passed
+        // Check Email
+        if(filter_var($email, FILTER_VALIDET_EMAIL) === false ){
+            // Failed
+            $msg =' Please Use a valide email';
+            $msgClass='alert-danger';
+        }else {network-p
+            // Passed
+            // Recipient Email
+            $to = 'orders@fortinet.ge';
+            $subject = 'Concact Request From'.$name;
+            $body = '<h2>Contact Reques</h2>
+            <h4>Name</h4><p>'.$name.'</p>;
+            <h4>Email</h4><p>'.$email.'</p>;
+            <h4>Message</h4><p>'.$message.'</p>';
+
+            //  Email Headers
+            $headers ="MIME-Version: 1.0" ."\r\n";
+            $headers .= "Content-Type:text/html;charset=UTF-8" . "\r\n";
+
+            // Additional Headers 
+            $headers .= "From:" .$name."<" .$email.">"."\r\n";
+            
+            if(mail($to,$subject,$body,$headers)){
+                // Email Sent 
+                $msg ='Your Email Has Been Sent';
+                $msgClass='alert-success';
+            }else {
+                // Failed 
+                $msg =' Your Email not Sent';
+                $msgClass='alert-danger';
+
+            }
+        }
+    }else {
+        // Failed
+        $msg =' Please Fill in All Fields';
+        $msgClass='alert-danger';
+    }
+}
+ ?>
